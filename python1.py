0b85405703ea2713b8da57fb91fe29b707c9b7b0
fadliwork55 <110509608+fadliwork55@users.noreply.github.com>
2022-08-08 08:51:14 +0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,13 @@
+# Belajar Output
+
+print("Hello World")
+print("Nama Saya Fadli")
+print("Saya kuliah di STT Nurul Fikri")
+print("Umur saya sudah tua")
+print("Hobi saya suka melawak")
+print("Kelemahan saya adalah lawakan saya garing")
+print("Cita-cita saya menjadi pendek")
+print("Saya mengajar Pemrograman Dasar")
+print("Rumah saya ditinggal")
+print("Saya pernah makan ricis sampe mata saya merah")
+print("ni ada buletan putih lagi di atas")
---
+++ python2.py
@@ -0,0 +1,28 @@
+# Belajar Input
+
+nama = "Fadli" # nama adalah variabel, Fadli adalah data
+umur = 20
+alamat = "cimanggis, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
+
+# print("Nama saya adalah " + nama + " Umur Saya " + str(umur) + " Alamat saya " + alamat)
+
+# Terdapat 4 Jenis Tipe Data :
+# 1. String adalah tipe data text 
+# yang ditandai dengan 
+# (" ") (tanda kutip dua) 
+# ('') kutip satu 
+# (''' ''') kutip tiga
+# 2. Integer adalah tipe data bilangan bulat. 
+# contoh: 1, 2, 10, 30, 1000
+# 3. Float adalah tipe data bilangan pecahan/desimal.
+# contoh: 1.5, 1.0, 2.8
+# 4. Boolean adalah tipe data yang memiliki dua nilai.
+# (benar/salah)
+# contoh: True / False
+
